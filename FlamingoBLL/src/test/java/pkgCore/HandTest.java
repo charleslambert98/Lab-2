6caee242d74258a44659f10c31dada312e3ec310
clambert <clambert@wifi-roaming-128-4-51-90.host.udel.edu>
2018-03-06 19:46:06 -0500

    HandTest Completed

--- FlamingoBLL/src/test/java/pkgCore/HandTest.java
+++ FlamingoBLL/src/test/java/pkgCore/HandTest.java
@@ -81,46 +81,79 @@ public class HandTest {
 
 		ArrayList<Card> cards = new ArrayList<Card>();
 		
-		// TODO:  REMOVE THE COMMENTS FROM THE NEXT THREE LINES
 		
-		//cards.add(new Card(eSuit.CLUBS,eRank.TWO));
-		//cards.add(new Card(eSuit.CLUBS,eRank.THREE));
-		//cards.add(new Card(eSuit.CLUBS,eRank.FOUR));
+		
+		cards.add(new Card(eSuit.CLUBS,eRank.TWO));
+		cards.add(new Card(eSuit.CLUBS,eRank.THREE));
+		cards.add(new Card(eSuit.CLUBS,eRank.FOUR));
 		
 		int[] score = HandHelper(cards);
 		
-		//	TODO: Check Score to make sure it's right.  Something close to this:
 		assertEquals(score[0],9);		
 	}
 
 	@Test
 	public void test2() {
-		// TODO: Test 2-3-J, score should be 15
+		cards.add(new Card(eSuit.CLUBS,eRank.TWO));
+		cards.add(new Card(eSuit.CLUBS,eRank.THREE));
+		cards.add(new Card(eSuit.CLUBS,eRank.JACK));
+		
+		assertEquals(score[0],15);
 	}
 
 	@Test
 	public void test3() {
-		// TODO: Test 2-3-J-J, score should be 25
+		cards.add(new Card(eSuit.CLUBS,eRank.TWO));
+		cards.add(new Card(eSuit.CLUBS,eRank.THREE));
+		cards.add(new Card(eSuit.CLUBS,eRank.JACK));
+		cards.add(new Card(eSuit.CLUBS,eRank.JACK));
+
+		
+		assertEquals(score[0],25);
 	}
 
 	@Test
 	public void test4() {
-		// TODO: Test J-A, score should be 21
+		cards.add(new Card(eSuit.CLUBS,eRank.JACK));
+		cards.add(new Card(eSuit.CLUBS,eRank.ACE));
+		
+		assertEquals(score[0],21);
+
+
 	}
 
 	@Test
 	public void test5() {
-		// TODO: Test J-A-A, score should be 12
+		cards.add(new Card(eSuit.CLUBS,eRank.JACK));
+		cards.add(new Card(eSuit.CLUBS,eRank.ACE));
+		cards.add(new Card(eSuit.CLUBS,eRank.ACE));
+
+		
+		assertEquals(score[0],12);
 	}
 
 	@Test
 	public void test6() {
-		// TODO: Test J-A-A-A, score should be 13
+		cards.add(new Card(eSuit.CLUBS,eRank.JACK));
+		cards.add(new Card(eSuit.CLUBS,eRank.ACE));
+		cards.add(new Card(eSuit.CLUBS,eRank.ACE));
+		cards.add(new Card(eSuit.CLUBS,eRank.ACE));
+
+
+		
+		assertEquals(score[0],13);
 	}
 	
 	@Test
 	public void test7() {
-		//	TODO: Test A-A-A-A-2, score should be 6 or 16
+		cards.add(new Card(eSuit.CLUBS,eRank.ACE));
+		cards.add(new Card(eSuit.CLUBS,eRank.ACE));
+		cards.add(new Card(eSuit.CLUBS,eRank.ACE));
+		cards.add(new Card(eSuit.CLUBS,eRank.ACE));
+		cards.add(new Card(eSuit.CLUBS,eRank.TWO));
+		
+		assertEquals(score[0],16);
+
 	}
 
 }
