8af43492ba6f2e7ce663f2b2d353c0f5c8f9a1fc
clambert <clambert@wifi-roaming-128-4-51-90.host.udel.edu>
2018-03-06 19:46:41 -0500

    TODOs Removed

--- FlamingoBLL/src/main/java/pkgCore/Hand.java
+++ FlamingoBLL/src/main/java/pkgCore/Hand.java
@@ -39,14 +39,6 @@ public class Hand {
 				}
 			}
 			iScore[0] += rankNumber;
-			//	TODO: Determine the score.  
-			//			Cards:
-			//			2-3-4 - score = 11
-			//			5-J-Q - score = 25
-			//			5-6-7-2 - score = 20
-			//			J-Q	- score = 20
-			//			8-A = score = 9 or 19
-			//			4-A = score = 5 or 15
 			
 		}
 		return iScore;
